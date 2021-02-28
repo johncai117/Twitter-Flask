f0f347dfed2f554d8e34d6db3f6615b5b033ec92
John Cai <johncai117@gmail.com>
2021-02-28 17:46:54 +0800

    rename

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ def get_data():
 @app.route('/success/<name>')
 def success(name):
     return "<xmp>" + str(requestResults(name)) + " </xmp> "
-
-app.run(debug=True)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
